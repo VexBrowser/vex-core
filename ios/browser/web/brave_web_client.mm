34a7cf4e25f8057c38e22442a12a073bb6f509e4
Stephen Heaps <steaps.4@gmail.com>
2026-07-15 11:09:01 -0400

    Add `ScriptletsJavaScriptFeature`.

--- ios/browser/web/brave_web_client.mm
+++ ios/browser/web/brave_web_client.mm
@@ -25,6 +25,7 @@
 #include "brave/ios/browser/brave_shields/cookie_control_javascript_feature.h"
 #include "brave/ios/browser/brave_shields/farbling_javascript_feature.h"
 #include "brave/ios/browser/brave_shields/request_blocking/request_blocking_javascript_feature.h"
+#include "brave/ios/browser/brave_shields/scriptlets/scriptlets_javascript_feature.h"
 #include "brave/ios/browser/global_privacy_control/gpc_javascript_feature.h"
 #include "brave/ios/browser/playlist/playlist_compatibility_javascript_feature.h"
 #include "brave/ios/browser/playlist/playlist_javascript_feature.h"
@@ -180,6 +181,7 @@ std::vector<web::JavaScriptFeature*> BraveWebClient::GetJavaScriptFeatures(
         skus::SkusJavaScriptFeature::FromBrowserState(browser_state));
     features.push_back(youtube::YouTubeQualityJavaScriptFeature::GetInstance());
     features.push_back(RequestBlockingJavaScriptFeature::GetInstance());
+    features.push_back(ScriptletsJavaScriptFeature::GetInstance());
     if (!base::FeatureList::IsEnabled(
             brave::features::kUseChromiumWebViewsAutofill)) {
       features.push_back(LoginsJavaScriptFeature::GetInstance());
